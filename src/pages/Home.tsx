03b6d9151f89947a1e7d924cb6320c6ba51bd090
saeedHak <hak@gmail.com>
2023-01-15 03:57:12 +0500

    finalHomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,49 +9,35 @@ const Tab1: React.FC = () => {
       <Header />
       <IonContent fullscreen>
         <div className="container bg-light pt-4 my__PB_3">
+
+          <div className="card shadow rounded-pill mb-3">
+            <div className="card-body p-1">
+              <div className="row g-0">
+                <div className="col-2 mt-1 ps-1 pe-1">
+                  <img src="https://ionicframework.com/docs/img/demos/avatar.svg" className="w-100 rounded-circle" alt="user" />
+                </div>
+                <div className="col-6">
+                  <p className="card-title m-0 text-dark" style={{ fontSize: "0.8rem", fontWeight: "bolder",lineHeight: "20px" }}>sign up or log in</p>
+                  <p className="card-text m-0" style={{ fontSize: "0.6rem", fontWeight: "500",lineHeight: "12px" }}>For better result for our new & Latest brand's product</p>
+                </div>
+                <div className="col-4 pe-1 mt-1">
+                    <div className="pt-3 pb-3  w-100 badge my__BG text-wrap rounded-pill text-center">
+                    Lets's go!
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="row">
             <div className="col-6">
-              <div className="card shadow border-2 my__Border">
-                <div className="card-body">
+              <div className="card shadow my__BOX_RADIUS">
+                <div className="card-body pe-0">
                   <h5 className="card-title my__textLeft my__headTitle mb-1 mt-0">Brands</h5>
                   <p className="card-text my__headSubTitle mt-0">Kuch bhai, khai bhi, kabhi bhi</p>
-                  <div className="row pt-4">
-                    <div className="col-12">
-                      <div className="col-12 mb-2">
-                        <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                          Iphone
-                        </div>
-                      </div>
-                      <div className="col-12 mb-2">
-                        <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                          Samsung
-                        </div>
-                      </div>
-                      <div className="col-12 mb-2">
-                        <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                          Audionic
-                        </div>
-                      </div>
-                      <div className="col-12 mb-2">
-                        <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                          Audionic
-                        </div>
-                      </div>
-                      <div className="col-12 mb-2">
-                        <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                          Audionic
-                        </div>
-                      </div>
-                      <div className="col-12 mb-2">
-                        <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                          Audionic
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row pt-3">
-                    <div className="col-12">
-                      <p className="text-center my__headSubTitle my__Border my__COLOR">See All</p>
+                  <div className="row pt-4 g-0">
+                    {/* <div className="col-2"></div> */}
+                    <div className="col-12 float-right p-0">
+                      <img src="../../assets/img/brand5.png" alt="Brand" />
                     </div>
                   </div>
                 </div>
@@ -59,57 +45,31 @@ const Tab1: React.FC = () => {
             </div>
             <div className="col-6 pl-0">
               <div className="row">
-                <div className="col-12 pb-2 ps-0">
-                  <div className="card shadow border-2 my__Border">
-                    <div className="card-body">
-                      <h5 className="card-title my__textLeft my__headTitle mb-1 mt-0">Category</h5>
-                      <p className="card-text my__headSubTitle mt-0">Kuch bhai, khai bhi, kabhi bhi</p>
-                      <div className="row pt-4">
-                        <div className="col-12">
-                          <div className="col-12 mb-2">
-                            <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                              Iphone
-                            </div>
-                          </div>
-                          {/* <div className="col-12 mb-2">
-                            <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                              Samsung
-                            </div>
-                          </div> */}
-                          
+                <div className="col-12 pb-2 ps-0 ">
+                  <div className="card shadow my__BOX_RADIUS">
+                    <div className="card-body pe-0 pb-2">
+                      <div className="row g-0">
+                        <div className="col-8">
+                          <h5 className="card-title my__textLeft my__headTitle mb-1 mt-0">Category</h5>
+                          <p className="card-text my__headSubTitle mt-0">Kuch bhai, khai bhi, kabhi bhi</p>
                         </div>
-                      </div>
-                      <div className="row pt-1">
-                        <div className="col-12">
-                          <p className="text-center my__headSubTitle my__Border my__COLOR">See All</p>
+                        <div className="col-12 text-end">
+                          <img className="w-50" src="../../assets/img/category.png" alt="Brand" />
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className="col-12 ps-0">
-                  <div className="card shadow border-2 my__Border">
-                    <div className="card-body">
-                      <h5 className="card-title my__textLeft my__headTitle mb-1 mt-0">Product</h5>
-                      <p className="card-text my__headSubTitle mt-0">Kuch bhai, khai bhi, kabhi bhi</p>
-                      <div className="row pt-4">
-                        <div className="col-12">
-                          <div className="col-12 mb-2">
-                            <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                              Iphone
-                            </div>
-                          </div>
-                          {/* <div className="col-12 mb-2">
-                            <div className="card pt-1 pb-1 shadow text-center rounded-pill my__headSubTitle my__BG my__Border">
-                              Samsung
-                            </div>
-                          </div> */}
-                          
+                  <div className="card shadow my__BOX_RADIUS">
+                    <div className="card-body pe-0 pb-0">
+                      <div className="row g-0">
+                        <div className="col-6">
+                          <h5 className="card-title my__textLeft my__headTitle mb-1 mt-0">Product</h5>
+                          <p className="card-text my__headSubTitle mt-0">Kuch bhai, khai bhi, kabhi bhi</p>
                         </div>
-                      </div>
-                      <div className="row pt-1">
-                        <div className="col-12">
-                          <p className="text-center my__headSubTitle my__Border my__COLOR">See All</p>
+                        <div className="col-6 text-end mt-3">
+                          <img className="w-100" src="../../assets/img/product.png" alt="Brand" />
                         </div>
                       </div>
                     </div>
